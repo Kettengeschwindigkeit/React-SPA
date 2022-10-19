3783c4e540a0a43618cee9d2a306e23f227d70d8
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-19 14:01:51 +0300

    Main page layout

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom'
 export function Navigation() {
     return (
         <nav className="flex justify-between items-center h-[50px] px-5 bg-gray-200 shadow-md">
-            Airport
-
+            <Link to="/">Airport</Link>
             <Link to="/auth">Auth</Link>
         </nav>
     )
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { AirportCard } from '../components/AirportCard'
+import { AirportFilter } from '../components/AirportFilter'
+import { AirportSearch } from '../components/AirportSearch'
 
 export function MainPage() {
     return (
-        <div>main</div>
+        <div className="container max-w-[760px] mx-auto pt-5">
+            <AirportSearch />
+            <AirportFilter />
+            <AirportCard />
+        </div>
     )
 }
