c5c8f8c0ef8d6626c2ff312da3b444a0015ed710
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-19 13:08:21 +0300

    Routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import logo from './logo.svg'
+import { Route, Routes } from 'react-router-dom'
+import { AirportDetailPage } from './pages/AirportDetailPage'
+import { AuthPage } from './pages/AuthPage'
+import { MainPage } from './pages/MainPage'
 
 function App() {
   return (
-    <div>hello</div>
+    <div>
+      <Routes>
+        <Route path="/" element={ <MainPage />} />
+        <Route path="/auth" element={ <AuthPage />} />
+        <Route path="/airport/:id" element={ <AirportDetailPage />} />
+      </Routes>
+    </div>
   )
 }
 
