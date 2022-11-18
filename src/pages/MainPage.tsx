20e51bc075f21320818e74c6f521a6534251cea8
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-18 11:50:12 +0300

    Loading data from server

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { AirportCard } from '../components/AirportCard'
 import { AirportFilter } from '../components/AirportFilter'
 import { AirportSearch } from '../components/AirportSearch'
+import { useAppDispatch } from '../hook/redux'
+import { fetchAirports } from '../store/actions/airportActions'
 
 export function MainPage() {
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(fetchAirports())
+    }, [])
+
     return (
         <div className="container max-w-[760px] mx-auto pt-5">
             <AirportSearch />
