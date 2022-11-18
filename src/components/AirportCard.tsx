1b051afeb1a7d01d04d354b1d84540eb70d486c8
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-18 16:06:51 +0300

    Stylization AirportCard

--- src/components/AirportCard.tsx
+++ src/components/AirportCard.tsx
@@ -1,4 +1,5 @@
 import { IAirport } from '../models/models'
+import classes from './AirportCard.module.css'
 
 interface AirportCardProps {
     airport: IAirport
@@ -6,8 +7,13 @@ interface AirportCardProps {
 
 export function AirportCard({ airport }: AirportCardProps) {
     return (
-        <div>
-            {airport.name}
+        <div className={classes.card}>
+            <p className='text-lg font-bold'>{airport.name}</p>
+            <p>{airport?.region}</p>
+            <p>{airport?.type}</p>
+            <p>{airport?.country}</p>
+            <p>{airport?.local_code}</p>
+            <p>{airport?.ident}</p>
         </div>
     )
 }
