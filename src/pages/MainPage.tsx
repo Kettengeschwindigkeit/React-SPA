b77f70cddf03297e9f4cf5f151b9371feb2a45dc
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-18 15:51:24 +0300

    Showing data in AirportCard

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react'
 import { AirportCard } from '../components/AirportCard'
 import { AirportFilter } from '../components/AirportFilter'
 import { AirportSearch } from '../components/AirportSearch'
-import { useAppDispatch } from '../hook/redux'
+import { useAppDispatch, useAppSelector } from '../hook/redux'
 import { fetchAirports } from '../store/actions/airportActions'
 
 export function MainPage() {
     const dispatch = useAppDispatch()
+
+    const { error, loading, airports } = useAppSelector(state => state.airport)
 
     useEffect(() => {
         dispatch(fetchAirports())
@@ -16,7 +18,7 @@ export function MainPage() {
         <div className="container max-w-[760px] mx-auto pt-5">
             <AirportSearch />
             <AirportFilter />
-            <AirportCard />
+            {airports.map(airport => <AirportCard key={airport.id} airport={airport} />)}
         </div>
     )
 }
--- src/store/actions/airportActions.ts
+++ src/store/actions/airportActions.ts
@@ -3,11 +3,11 @@ import { AppDispatch } from '../index'
 import { IAirport, ServerResponse } from '../../models/models'
 import { airportSlice } from '../slices/airportSlice'
 
-export const fetchAirports = () => {
+export const fetchAirports = (page = 1, count = 50) => {
     return async (dispatch: AppDispatch) => {
         try {
             dispatch(airportSlice.actions.fetching())
-            const response = await axios.get<ServerResponse<IAirport>>('airports')
+            const response = await axios.get<ServerResponse<IAirport>>('airports', { params: { page, count } })
             dispatch(airportSlice.actions.fetchSuccess(response.data.results))
         } catch (e) {
             dispatch(airportSlice.actions.fetchError(e as Error))
